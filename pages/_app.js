3f5083aa32da7490fd3860f93c301b8c9940fc18
= <=>
2021-06-05 22:43:27 +0200

    Add fathom analytics to project

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,30 @@
 import "@fontsource/press-start-2p";
 
+import {useEffect} from 'react';
+import {useRouter} from 'next/router';
+import * as Fathom from 'fathom-client';
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    Fathom.load('TBSAIDMS', {
+      includedDomains: ['twan.dev'],
+    })
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [])
+
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
